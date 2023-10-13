daa261666e21fa61f45101d7e45278d46494ba55
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-10-13 14:44:56 +0100

    style: Add styling for HangmanDrawing component

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
+const HEAD = (
+    <div style={{
+        width: "50px",
+        height: "50px",
+        borderRadius: "100%",
+        border: "10px solid black",
+        position: "absolute",
+        right: "-30px",
+        top: "50px"
+    }} />
+
+)
+
 const HangmanDrawing = () => {
     return (
         <div style={{ position: "relative" }}>
-            <div style={{ borderLeft: "5px solid black",backgroundColor:"red" }} ></div>
-            <hr style={{ width: "100px", height: "5px", backgroundColor: "black" }} />
+            {HEAD}
+            <div style={{ width: "10px", height: "50px", backgroundColor: "black", top: 0, right: 0, position: "absolute" }} />
+            <div style={{ width: "200px", height: "10px", backgroundColor: "black", marginLeft: "120px" }} />
+            <div style={{ height: "400px", background: "black", width: "10px", marginLeft: "120px" }} />
+            <div style={{ width: "250px", height: "10px", backgroundColor: "black" }} />
         </div>
     )
 }

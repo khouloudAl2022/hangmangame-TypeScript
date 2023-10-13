641e246908f0d7546492611b7ff0c23e110007a9
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-10-13 15:41:37 +0100

    The hangman includes the head, body, arms, and legs, creating an initial visual structure for the Hangman game

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -12,11 +12,78 @@ const HEAD = (
     }} />
 
 )
+const BODY = (
+    <div style={{
+        width: "10px",
+        height: "100px",
+        background: "black",
+        position: "absolute",
+        right: "0px",
+        top: "120px"
+    }} />
+
+)
+const RIGHT_ARM = (
+    <div style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        right: "-100px",
+        top: "150px",
+        rotate: "-30deg",
+        transformOrigin: "left bottom"
+    }} />
+
+)
+const LEFT_ARM = (
+    <div style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        right: "10px",
+        top: "150px",
+        rotate: "30deg",
+        transformOrigin: "right bottom"
+    }} />
+)
+const LEFT_LEG = (
+    <div style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        right: "0px",
+        top: "210px",
+        rotate: "-60deg",
+        transformOrigin: "right bottom"
+    }} />
+)
+
+const RIGHT_LEG = (
+    <div style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        right: "-90px",
+        top: "210px",
+        rotate: "60deg",
+        transformOrigin: " left bottom"
+    }} />
+)
+
 
 const HangmanDrawing = () => {
     return (
         <div style={{ position: "relative" }}>
             {HEAD}
+            {BODY}
+            {RIGHT_ARM}
+            {LEFT_ARM}
+            {RIGHT_LEG}
+            {LEFT_LEG}
             <div style={{ width: "10px", height: "50px", backgroundColor: "black", top: 0, right: 0, position: "absolute" }} />
             <div style={{ width: "200px", height: "10px", backgroundColor: "black", marginLeft: "120px" }} />
             <div style={{ height: "400px", background: "black", width: "10px", marginLeft: "120px" }} />
@@ -24,5 +91,6 @@ const HangmanDrawing = () => {
         </div>
     )
 }
+
 
 export default HangmanDrawing
--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const HangmanWord = () => {
-  return (
-    <div>
-      hangmanword
-    </div>
-  )
+    const word = "test"
+    return (
+        <div style={{
+            display: "flex",
+            gap: ".25rem",
+            fontSize: "6rem",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            fontFamily: "monospace"
+        }}>
+            {word.split('').map((letter, index) =>
+            (<span style={{borderBottom:"10px solid"}}> {letter}</span>
+            ))}</div>
+    )
 }
 
 export default HangmanWord

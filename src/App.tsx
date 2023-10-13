9c46503b983f330d6b2ff76a7e2d7ea2dc584400
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-10-13 13:41:27 +0100

    feat: Create the initial structure of the Hangman game sets up the basic structure for the Hangman game, it initializes the word to guess, manages guessed letters, and provides a container for other game component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react"
+import wordList from "./wordList.json"
+import HangmanDrawing from "./components/HangmanDrawing"
+import Keyboard from "./components/Keyboard"
+import HangmanWord from "./components/HangmanWord"
 
 function App() {
 
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return wordList[Math.floor(Math.random() * wordList.length)]
+  })
+  console.log("guessed word :", wordToGuess)
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+
   return (
-    <h1>
-      Hello
-    </h1>
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center"
+    }}>
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        Lose - Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
   )
 }
 
---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HangmanDrawing = () => {
+    return (
+        <div style={{ position: "relative" }}>
+            <div style={{ borderLeft: "5px solid black",backgroundColor:"red" }} ></div>
+            <hr style={{ width: "100px", height: "5px", backgroundColor: "black" }} />
+        </div>
+    )
+}
+
+export default HangmanDrawing

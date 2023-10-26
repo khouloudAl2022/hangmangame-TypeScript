16014ff3889eed5c44453313e67386795a208fe9
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-10-26 16:17:54 +0100

    feat: Adding props to keyBoard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import wordList from "./wordList.json"
 import HangmanDrawing from "./components/HangmanDrawing"
 import Keyboard from "./components/Keyboard"
@@ -16,17 +16,19 @@ function App() {
   const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
 
 
-  const addGuessedLetter = (letter: string) => {
+  const addGuessedLetter = useCallback((letter: string) => {
     if (guessedLetters.includes(letter)) return
-    setGuessedLetters(currentletters => [...currentletters, letter])
-  }
+    setGuessedLetters(currentletters => [...currentletters, letter]); {/*Managing the list of guessed letters and updating the game state*/ }
+  }, [guessedLetters])
+
+
 
 
   // useEffect for handling keyboard event to get the guessedLetters
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
       const key = e.key
-      if (!key.match(/^[a-z]$/)) return
+      if (!key.match(/^[a-z]$/)) return {/* Preventing non-alphabet characters from being processed*/ }
       e.preventDefault()
       addGuessedLetter(key)
     }
@@ -34,7 +36,7 @@ function App() {
     return () => {
       document.removeEventListener("keypress", handler)
     }
-  }, [])
+  }, [guessedLetters])
 
   return (
     <div style={{
@@ -51,7 +53,7 @@ function App() {
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
       <div style={{ alignSelf: "stretch" }}>{/*Stretch the item to fill the vertical space*/}
-        <Keyboard />
+        <Keyboard activeLetters={guessedLetters.filter(letter => wordToGuess.includes(letter))} inactiveLetters={incorrectLetters} addGuessedLetter={addGuessedLetter} />
       </div>
     </div>
   )
